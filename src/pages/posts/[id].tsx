7e3f23d061cecb36380e0566abbe848ca4023f9f
gcode101 <gcode101@users.noreply.github.com>
2024-12-19 13:06:02 -0500

    added some styling to the post page

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -12,11 +12,19 @@ type PostProps = {
 
 const PostPage = ({ post }: PostProps) => {
     return (
-        <div>
-            <Link href="/posts">Back to posts</Link>
-            <h1>{post.title}</h1>
-            <p>{post.published}</p>
-            <div>{post.content}</div>
+        <div className="p-5">
+            <Link href="/posts" className="relative inline-flex items-center justify-center p-0.5 mb-4 overflow-hidden text-md font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Back to posts
+                </span>
+            </Link>            
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">{post.title}</h1>
+            <p className="text-sm text-gray-500">{new Date(post.published).toLocaleDateString()}</p>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: post.content
+                }}
+            ></div>
         </div>
     )
 }
